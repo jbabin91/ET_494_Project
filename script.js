c9ca4d694ab3dab2c248af18ca9d5981682f90f0
Andrew Brouillette <a.brouillette94@gmail.com>
2017-01-02 17:15:26 -0600

    Added data measured from position coordinates and added new steps to try under last TODO comment

--- script.js
+++ script.js
@@ -84,28 +84,28 @@ Cylon.robot({
             var middleY = middlePosition[1];
             var ringY = ringPosition[1];
             var pinkyY = pinkyPosition[1];
-            var thumbZ = thumbPosition[2];
+          /*  var thumbZ = thumbPosition[2];
             var indexZ = indexPosition[2];
-            var middleZ = middlePosition[2];
+            var middleZ = middlePosition[2];  //shouldnt need z position but included it just incase
             var ringZ = ringPosition[2];
-            var pinkyZ = pinkyPosition[2];
+            var pinkyZ = pinkyPosition[2];*/
 
 
 
             //printing the finger positions
-            // console.log(
-            //     " Thumb X Position: " + thumbPosition[0],
-            //     "Index X Position: " + indexPosition[0],
-            //     "middle X Position: " + middlePosition[0],
-            //     "ring X Position: " + ringPosition[0],
-            //     "pinky X Position: " + pinkyPosition[0], //data is stored[x,y,z]. green light facing user on leap is proper position
-            //         "\n",
-            //     "Thumb Y Position: " + thumbPosition[1],
-            //     "Index Y Position: " + indexPosition[1],
-            //     "middle Y Position: " + middlePosition[1],
-            //     "ring Y Position: " + ringPosition[1],
-            //     "pinky Y Position: " + pinkyPosition[1],
-            //          "\n",
+             console.log(// data is stored [x,y,z]
+                 " Thumb X Position: " + thumbPosition[0],  // -62.4 position of thumb at aprox 6 inches from base of ruler to base of thumb---goes less negative  when moving right
+                 "Index X Position: " + indexPosition[0],// -41.3 ''''
+                 "middle X Position: " + middlePosition[0],//-21'''''
+                 "ring X Position: " + ringPosition[0],//-2.1''''
+                 "pinky X Position: " + pinkyPosition[0],//27.2 - 27.35(positive)''''
+                    "\n",
+                 "Thumb Y Position: " + thumbPosition[1],//140.9-141.5(positive)''''
+                 "Index Y Position: " + indexPosition[1],//151-162(positive)''''
+                 "middle Y Position: " + middlePosition[1],//160.3-160.45(positive)''''
+                 "ring Y Position: " + ringPosition[1],//163.2-163.34(positive)''''
+                 "pinky Y Position: " + pinkyPosition[1],//162.2-162.5(positive)''''
+                     "\n");
             //     "Thumb Z Position: " + thumbPosition[2],
             //     "Index Z Position: " + indexPosition[2],
             //     "middle Z Position: " + middlePosition[2],
@@ -124,7 +124,7 @@ Cylon.robot({
              //thumb finger
 
 
-           if(thumbVelocity[0] >= 20) {  //when green light on LEAP is facing the user, right is positive, up is negative, down and left is negative, down and right is postive, up and right is postive, up and left is negative
+          /* if(thumbVelocity[0] >= 20) {  //when green light on LEAP is facing the user, right is positive, up is negative, down and left is negative, down and right is postive, up and right is postive, up and left is negative
                 console.log("thumb moving right");//green light facing user--x right is postive, y up is positve, z forward push is negative
                 // " Thumb X Position: " + thumbPosition[0],
                 // "Thumb Y Position: " + thumbPosition[1],
@@ -141,12 +141,12 @@ Cylon.robot({
             if(thumbVelocity[1] <= -20){
                 console.log("thumb moving down");
             }
-           /* if(thumbVelocity[2] >= 20){
+           if(thumbVelocity[2] >= 20){
                 console.log("thumb moving backwards");
             }
             if(thumbVelocity[2] <= -20){
                 console.log("thumb moving forwards");
-            }*/
+            }
 
 
             //index finger
@@ -165,12 +165,12 @@ Cylon.robot({
             if(indexVelocity[1] <= -20){
                 console.log("index moving down");
             }
-         /*   if(indexVelocity[2] >= 20){
+            if(indexVelocity[2] >= 20){
                 console.log("index moving backwards");
             }
             if(indexVelocity[2] <= -20) {
                 console.log("index moving forwards");
-            }*/
+            }
 
                 //middle finger
 
@@ -188,12 +188,12 @@ Cylon.robot({
                 if(middleVelocity[1] <= -20){
                     console.log("middle moving down");
                 }
-               /* if(middleVelocity[2] >= 20){
+                if(middleVelocity[2] >= 20){
                     console.log("middle moving backwards");
                 }
                 if(middleVelocity[2] <= -20) {
                     console.log("middle moving forwards");
-                }*/
+                }
 
                     //ring finger
 
@@ -211,12 +211,14 @@ Cylon.robot({
                     if(ringVelocity[1] <= -20){
                         console.log("ring moving down");
                     }
-                   /* if(ringVelocity[2] >= 20){
+                    if(ringVelocity[2] >= 20){
                         console.log("ring moving backwards");
                     }
                     if(ringVelocity[2] <= -20) {
                         console.log("ring moving forwards");
-                    }*/
+                    }
+
+
                         //pinky finger
 
 
@@ -233,12 +235,12 @@ Cylon.robot({
                         if(pinkyVelocity[1] <= -20){
                             console.log("pinky moving down");
                         }
-                      /*  if(pinkyVelocity[2] >= 20){
+                        if(pinkyVelocity[2] >= 20){
                             console.log("pinky moving backwards");
                         }
                         if(pinkyVelocity[2] <= -20) {
                             console.log("pinky moving forwards");
-                        }*/
+                        }
 
 
                         //multiple positions
@@ -326,9 +328,18 @@ Cylon.robot({
             }
             if(pinkyVelocity[0] <= -20 && pinkyVelocity[1] >= 20){
                 console.log("pinky moving up towards the left");
-            }
+            }*/
 
 
+
+
+          //TODO
+            //try this
+            //create functions for the positive x and negative x axis for each finger
+            //create functions for the positive and negative y axis for each finger
+            //use the position to determine the average "origin" of each finger
+            //when said finger moves either more negative or more positive send to appropriate function
+            //come up with servo control from there
 
 
         });
